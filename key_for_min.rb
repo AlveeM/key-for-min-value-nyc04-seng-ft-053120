06aa043ee74ed3be279b25459052c355783cd602
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-02 20:57:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  return nil if name_hash.empty?
   
+  name_hash.reduce do |min, cur|
+    min = cur if cur < min
+    min
 end
